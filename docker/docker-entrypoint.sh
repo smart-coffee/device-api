4493cf1e076a5cb8ef8f1b6a3f237687e4a82f69
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-02 23:11:23 +0100

    [ADD] Docker stuff

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd src
+
+env_file=".env"
+if [ ! -e "$env_file" ]
+then
+        cat <<EOF >$env_file
+MODE=prod
+
+# Should be empty in $local_mode environment
+CERT_FILE=
+KEY_FILE=
+
+# Customize as needed
+APP_HOST=0.0.0.0
+APP_PORT=80
+SECRET_KEY=XLrIjHvKsQskA7m
+APP_URL_PREFIX=/api
+
+# Should be empty in $local_mode environment
+SWAGGER_BASE_URL=
+EOF
+fi
+
+python -u app.py
