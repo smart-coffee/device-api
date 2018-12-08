7b9acd7411a68af326881305a5a4dce476a70ab0
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-08 12:21:17 +0100

    [FIX] Os environ error

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,8 +9,8 @@ then
 MODE=prod
 
 # Should be empty in $local_mode environment
-CERT_FILE=
-KEY_FILE=
+CERT_FILE=""
+KEY_FILE=""
 
 # Customize as needed
 APP_HOST=0.0.0.0
@@ -19,7 +19,7 @@ SECRET_KEY=XLrIjHvKsQskA7m
 APP_URL_PREFIX=/api
 
 # Should be empty in $local_mode environment
-SWAGGER_BASE_URL=
+SWAGGER_BASE_URL=""
 
 WEBAPI_DOMAIN=https://tobias-blaufuss.de
 WEBAPI_PORT=65291
