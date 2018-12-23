5a5328a051d0b33d8faea55865ea9d82570ac220
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-23 20:14:28 +0100

    [UPDATE] coffee strength DAC output

---
+++ src/utils/basic.py
@@ -0,0 +1,19 @@
+def validate_percent_value(value: int, accuracy: int = 0) -> bool:
+    is_percent_value = is_percent_value(value=value, accuracy=accuracy)
+    if not is_percent_value:
+        raise ValueError('Not a valid percent value: {0} (Accuracy: {1})'.format(value, accuracy))
+
+
+def is_percent_value(value: int, accuracy: int = 0) -> bool:
+    if accuracy < 0:
+        raise ValueError('Accuracy has to be greater or equal to zero'.)
+    
+    if value < 0:
+        raise ValueError('Percent value has to be greater or equal to zero.')
+    
+    divisor = 100 * (10 ** accuracy)
+    value = int(value / divisor)
+    in_range = value in range(0, 101)
+    
+    return in_range
+
