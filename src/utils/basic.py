3ef8cf0f5ec0c4653fa1cf288edb14c7d88f243b
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-23 22:10:54 +0100

    [UPDATE] debug

--- src/utils/basic.py
+++ src/utils/basic.py
@@ -1,3 +1,9 @@
+from config.logger import logging, get_logger_name
+
+
+logger = logging.getLogger(get_logger_name(__name__))
+
+
 def validate_percent_value(value: int, accuracy: int = 0) -> bool:
     _is_percent_value = is_percent_value(value=value, accuracy=accuracy)
     if not _is_percent_value:
@@ -14,6 +20,8 @@ def is_percent_value(value: int, accuracy: int = 0) -> bool:
     divisor = 100 * (10 ** accuracy)
     value = int(value / divisor)
     in_range = value in range(0, 101)
+
+    logger.debug('{0} {1} {2}'.format(divisor, value, in_range))
     
     return in_range
 
