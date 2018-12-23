2ce812572a1db5938b1860450709d9e911dff5f6
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-23 22:41:09 +0100

    [FIX] accuracy

--- src/utils/basic.py
+++ src/utils/basic.py
@@ -4,13 +4,22 @@ from config.logger import logging, get_logger_name
 logger = logging.getLogger(get_logger_name(__name__))
 
 
-def validate_percent_value(value: int, accuracy: int = 0) -> bool:
+def validate_percent_value(value: int, accuracy: int = 0):
     _is_percent_value = is_percent_value(value=value, accuracy=accuracy)
     if not _is_percent_value:
         raise ValueError('Not a valid percent value: {0} (Accuracy: {1})'.format(value, accuracy))
 
 
 def is_percent_value(value: int, accuracy: int = 0) -> bool:
+    value = get_percent_value(value, accuracy)
+    in_range = value in range(0, 101)
+
+    logger.debug('Accuracy: {0}, Value: {1}, In Range: {2}'.format(divisor, value, in_range))
+    
+    return in_range
+
+
+def get_percent_value(value: int, accuracy: int = 0) -> int:
     if accuracy < 0:
         raise ValueError('Accuracy has to be greater or equal to zero.')
     
@@ -19,9 +28,4 @@ def is_percent_value(value: int, accuracy: int = 0) -> bool:
     
     divisor = 10 ** accuracy
     value = int(value / divisor)
-    in_range = value in range(0, 101)
-
-    logger.debug('Accuracy: {0}, Value: {1}, In Range: {2}'.format(divisor, value, in_range))
-    
-    return in_range
-
+    return value
