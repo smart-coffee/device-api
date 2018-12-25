3cd52aec92388c83de6692b3614090229f100650
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-26 00:46:03 +0100

    [TEST] gpio input

--- src/main.py
+++ src/main.py
@@ -8,15 +8,30 @@
 
 #####
 
-from smbus2 import SMBus
+# from smbus2 import SMBus
 
-bus = SMBus(1)
-# Register addresses (with "normal mode" power-down bits)
-reg_write_dac = 0x40
-address = 0x60
-voltage = 0xaaa
+# bus = SMBus(1)
+# # Register addresses (with "normal mode" power-down bits)
+# reg_write_dac = 0x40
+# address = 0x60
+# voltage = 0xaaa
 
-msg = (voltage & 0xff0) >> 4
-msg = [msg, (msg & 0xf) << 4]
+# msg = (voltage & 0xff0) >> 4
+# msg = [msg, (msg & 0xf) << 4]
 
-bus.write_i2c_block_data(address, reg_write_dac, msg)
+# bus.write_i2c_block_data(address, reg_write_dac, msg)
+
+import RPi.GPIO as GPIO
+import time
+
+
+pin = 20
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+for i in range(0, 1001):
+    _val = GPIO.input(pin)
+    _i_str = str(i).zfill(4)
+    _time_in_milli = int(round(time.time() * 1000))
+    print('{0}: {1} {2}'.format(_i_str, _val, _time_in_milli)
