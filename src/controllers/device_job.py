b31e9161349c277977f8e11aaea9f13792c5045b
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-25 20:30:43 +0100

    [UPDATE] Logging + accuracy=0

--- src/controllers/device_job.py
+++ src/controllers/device_job.py
@@ -4,6 +4,11 @@ from config.flask_config import ResourceException
 from utils.basic import validate_percent_value, get_percent_value
 
 
+from config.logger import logging, get_logger_name
+
+
+logger = logging.getLogger(get_logger_name(__name__))
+
 class DeviceJobController:
     def create_job(self, token:str, create_job: CreateDeviceJob) -> DeviceJob:
         status = CM_API.status
@@ -15,10 +20,11 @@ class DeviceJobController:
             **(settings.__dict__)
         }
         water_in_percent = create_job.water_in_percent
-        water_in_percent = get_percent_value(value=water_in_percent, accuracy=3)
+        water_in_percent = get_percent_value(value=water_in_percent, accuracy=0)
         coffee_strength_in_percent = create_job.coffee_strength_in_percent
         coffee_strength_in_percent = get_percent_value(value=coffee_strength_in_percent, accuracy=0)
         doses = create_job.doses
+        logger.debug('Doses: {0}, Water: {1}, Coffee: {2}'.format(doses, water_in_percent, coffee_strength_in_percent))
         CM_API.set_water_in_percent(water_in_percent=water_in_percent)
         CM_API.set_coffee_strength_in_percent(coffee_strength_in_percent=coffee_strength_in_percent)
         CM_API.make_coffee(doses=doses)
