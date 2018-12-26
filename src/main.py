ec653079160c6fa1009fa9bbb4ca9681c25b4852
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-26 19:41:56 +0100

    [UPDATE] Python 1000 read cycles

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@
 # msg = [msg, (msg & 0xf) << 4]
 
 # bus.write_i2c_block_data(address, reg_write_dac, msg)
-
+from typing import List
 import RPi.GPIO as GPIO
 import time
 
@@ -38,6 +38,29 @@ class GPIORead:
         value_str = self.value
         return '{0} {1}: {2}'.format(gpio_number_str, time_str, value_str)
 
+    def __eq__(self, other):
+        gpio_number_other = other.gpio_number
+        gpio_number_self = self.gpio_number
+        if gpio_number_self != gpio_number_other:
+            return False
+        
+        value_other = other.value
+        value_self = self.value
+        return value_self == value_other
+    
+    def __hash__(self):
+        return hash((self.gpio_number, self.value))
+    
+    def __lt__(self, other):
+        gpio_number_other = other.gpio_number
+        gpio_number_self = self.gpio_number
+        return gpio_number_self < gpio_number_other
+    
+    def __gt__(self, other):
+        gpio_number_other = other.gpio_number
+        gpio_number_self = self.gpio_number
+        return gpio_number_self < gpio_number_other
+
 
 gpio_numbers = [4, 17, 27, 22, 5, 6, 13, 19]
 
@@ -46,23 +69,44 @@ GPIO.setmode(GPIO.BCM)
 for gpio in gpio_numbers:
     GPIO.setup(gpio, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-reads = []
 
-for gpio in gpio_numbers:
-    _found_high = False
-    for i in range(0, 100):
-        _val = GPIO.input(gpio)
-        if _val == GPIO.HIGH:
-            _found_high = True
+def read_status(check_cycles: int = 1) -> List[GPIORead]:
+    if check_cycles < 1:
+        raise ValueError('Check cycles must be greater than 0')
+    
+    read_cycles = []
+    for i in range(0, check_cycles):
+        single_read = single_read_cycle()
+        read_cycles.append(single_read)
+    
+    base_cycle = read_cycles[0]
+    del read_cycles[0]
+
+    for read_cycle in read_cycles:
+        base_cycle = set(base_cycle).intersection(read_cycle)
+    
+    return base_cycle
+
+    
+def single_read_cycle() -> List[GPIORead]:
+    reads = []
+    for gpio in gpio_numbers:
+        _found_high = False
+        for i in range(0, 100):
+            _val = GPIO.input(gpio)
+            if _val == GPIO.HIGH:
+                _found_high = True
+                _time_in_milli = int(round(time.time() * 1000))
+                reads.append(GPIORead(gpio_number=gpio, value=True, time_in_milli=_time_in_milli))
+                break
+        if not _found_high:
             _time_in_milli = int(round(time.time() * 1000))
-            reads.append(GPIORead(gpio_number=gpio, value=True, time_in_milli=_time_in_milli))
-            break
-    if not _found_high:
-        _time_in_milli = int(round(time.time() * 1000))
-        reads.append(GPIORead(gpio_number=gpio, value=False, time_in_milli=_time_in_milli))
+            reads.append(GPIORead(gpio_number=gpio, value=False, time_in_milli=_time_in_milli))
+    
+    return reads
 
 
 
-
+reads = read_status(5)
 for i in reads:
     print(i)
