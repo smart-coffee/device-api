58aaa0bafcb1e71d6f781851dfc9d958d7e1d23a
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-12-26 16:37:23 +0100

    [ADD] C library first version + compile

---
+++ c_lib/read_gpio.c
@@ -0,0 +1,49 @@
+/*
+   pulse.c
+
+   gcc -o pulse pulse.c -lpigpio -lrt -lpthread
+
+   sudo ./pulse
+*/
+
+#include <stdio.h>
+#include <pigpio.h>
+
+
+int main(int argc, char *argv[])
+{
+   double start;
+   int pin = 20;
+
+   if (gpioInitialise() < 0)
+   {
+      fprintf(stderr, "pigpio initialisation failed\n");
+      return 1;
+   }
+
+   /* Set GPIO modes */
+   gpioSetMode(pin, PI_INPUT);
+
+
+//    start = time_time();
+
+//    while ((time_time() - start) < 60.0)
+//    {
+//       gpioWrite(18, 1); /* on */
+
+//       time_sleep(0.5);
+
+//       gpioWrite(18, 0); /* off */
+
+//       time_sleep(0.5);
+
+//       /* Mirror GPIO24 from GPIO23 */
+//       gpioWrite(24, );
+//    }
+    printf(gpioRead(pin));
+
+   /* Stop DMA, release resources */
+   gpioTerminate();
+
+   return 0;
+}
